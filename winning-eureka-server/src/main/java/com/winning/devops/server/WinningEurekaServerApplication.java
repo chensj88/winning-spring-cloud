1d96863ae3c0b1d287a2d84a9b5f8326d92982e5
chensj.dev <chenshijie1988@yeah.net>
2019-04-10 21:07:22 +0800

    初始化 Eureka Server项目

---
+++ winning-eureka-server/src/main/java/com/winning/devops/server/WinningEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.winning.devops.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinningEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinningEurekaServerApplication.class, args);
+    }
+
+}
