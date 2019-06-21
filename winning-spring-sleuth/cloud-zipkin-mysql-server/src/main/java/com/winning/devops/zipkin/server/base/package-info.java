54d82989dfbaed71708e3568ec697a908ed6d976
chensj.dev <chenshijie1988@yeah.net>
2019-06-21 22:22:23 +0800

    项目整合

--- winning-spring-sleuth/cloud-zipkin-mysql-server/src/main/java/com/winning/devops/zipkin/server/base/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author chensj
- * @title
- * @project winning-spring-cloud
- * @package com.winning.devops.zipkin.server.base
- * @date: 2019-04-29 22:23
- */
-package com.winning.devops.zipkin.server.base;
